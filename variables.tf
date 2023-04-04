74912ece37df0d40ab8291900ccda552533ffc6b
Nigel Alvaro Sadeli <43457221+NigelAlvaro@users.noreply.github.com>
2023-04-03 21:48:54 -0700

    Update #4

--- variables.tf
+++ variables.tf
@@ -12,23 +12,6 @@ variable "credentials" {
   description = "Credentials needed to connect to my AWS account"
 }
 
-locals {
-  os_type = {
-    linux = "amzn2-ami-kernel-5.10-hvm-*-x86_64-gp2"
-    ubuntu = "ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20230325"
-    windows = "Windows_Server-2022-English-Full-Base-2023.03.15"
-  }
-}
-
-data "aws_ami" "ami_type" {
-  most_recent = true
-  owners = ["amazon"]
-  filter {
-    name   = "name"
-    values = [local.os_type["${var.os_type}"]]
-  }
-}
-
 variable "ingress" {
   type = object({
     cidr_blocks = list(string)
